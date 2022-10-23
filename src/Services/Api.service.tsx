bff8e69036abe26048369be503d402be88d42a9c
FacaHD <sousadaniel948@gmail.com>
2022-10-23 15:56:19 +0100

    commit inicial, com serviço api e login page

---
+++ src/Services/Api.service.tsx
@@ -0,0 +1,59 @@
+import axios from "axios"
+
+
+const config = {
+    api: 'http://localhost:6532',
+    options: {
+      headers: { 'content-type': 'application/json' },
+    },
+  };
+
+
+
+const httpGet = (endpoint:string) => {
+  return new Promise((resolve,reject) => { axios.get(`${config.api}${endpoint}`,config.options)
+    .then((response) => {resolve (response)})
+    .catch((error) => {
+    console.error(error);
+    reject (error);
+  }).finally(); 
+}
+)
+};
+
+const httpPost=(endpoint:string,data:any) => {
+    return new Promise((resolve,reject) => { axios.post(`${config.api}${endpoint}`, data,config.options)
+        .then((response) => {resolve (response)})
+        .catch((error) => {
+          console.error(error);
+          reject (error);
+        }).finally(); 
+      }
+    )
+    
+}
+
+const httpPut = (endpoint:string, data:any) => {
+  return new Promise((resolve,reject) => { axios.put(`${config.api}${endpoint}`, data,config.options)
+    .then((response) => {resolve (response)})
+    .catch((error) => {
+    console.error(error);
+    reject (error);
+  }).finally(); 
+}
+)
+  };
+
+
+const httpDelete = (endpoint:string, data:any) => {
+  return new Promise((resolve,reject) => { axios.post(`${config.api}${endpoint}`, data,config.options)
+    .then((response) => {resolve (response)})
+    .catch((error) => {
+    console.error(error);
+    reject (error);
+  }).finally(); 
+}
+)
+  };
+
+export default { httpGet, httpPost, httpPut, httpDelete };
