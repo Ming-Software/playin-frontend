52073a4d774ef3c5ec412a482238c81633321257
sajoao23 <santossajoaomiguel@gmail.com>
2022-11-04 17:39:53 +0000

    branch com signup

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/Login";
 import AboutPage from "./Pages/About";
+import SignUpPage from "./Pages/Signup";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<LoginPage />} />
-                <Route path="/about" element={<AboutPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
