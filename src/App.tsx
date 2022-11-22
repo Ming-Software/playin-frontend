4a2e5fc093ae69037573219b747c994ae2dbcf06
João Amorim <jonny.amorim2000@gmail.com>
2022-11-22 22:12:50 +0000

    comit merdas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import LoginPage from "./Pages/Login";
 import AboutPage from "./Pages/About";
 import SignUpPage from "./Pages/Signup";
 import NewEvent from "./Pages/NewEvent";
+import Event from "./Components/Event/Event";
+import { EventsPage } from "./Pages/Events";
 
 const App = () => {
   return (
@@ -13,6 +15,46 @@ const App = () => {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/newevent" element={<NewEvent />} />
+        <Route
+          path="/events"
+          element={
+            /*<Event
+              eventProps={{
+                nameEvent: "jogo",
+                creator: "joni",
+                local: "braga",
+                date: "1/12/2022",
+                startTime: "10:00",
+                endTime: "11:00",
+                description: "jogo de preparação",
+                maxParticipants: 10,
+                actualParticipants: 5,
+                social_label: "Competitivo",
+                sport_label: "Basquete",
+              }}
+            />*/ <EventsPage />
+          }
+        />
+        <Route
+          path="/event"
+          element={
+            <Event
+              eventProps={{
+                nameEvent: "jogo",
+                creator: "joni",
+                local: "braga",
+                date: "1/12/2022",
+                startTime: "10:00",
+                endTime: "11:00",
+                description: "jogo de preparação",
+                maxParticipants: 10,
+                actualParticipants: 5,
+                social_label: "Competitivo",
+                sport_label: "Basquete",
+              }}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
