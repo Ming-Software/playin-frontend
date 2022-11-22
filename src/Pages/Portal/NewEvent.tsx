c3e14f778d9eb5845324391c3e271165694e6411
sajoao23 <santossajoaomiguel@gmail.com>
2022-11-22 16:19:11 +0000

    variavéis todas guardadas e erros nas caixas de texto adicionados, falta fazer o post para o backend

--- src/Pages/Portal/NewEvent.tsx
+++ src/Pages/Portal/NewEvent.tsx
@@ -1,31 +1,42 @@
 import bulmaCalendar from "bulma-calendar";
-import React, { useState } from "react";
+import React, { ReactText, useState } from "react";
 
 export const NewEvent = () => {
   const [name, setName] = useState("");
+  const [errorName, setErrorName] = useState(false);
+
   const [description, setDescription] = useState("");
+  const [errorDescription, setErrorDescription] = useState(false);
+  const [startData, setStartData] = useState("");
   const [startTime, setStartTime] = useState("");
+  const [finishData, setFinishData] = useState("");
   const [finishTime, setFinishTime] = useState("");
   const [publico, setPublic] = useState(false);
+
   const [maxUsers, setMaxUsers] = useState(0);
+  const [errormaxUsers, setErrorMaxUsers] = useState(false);
   const [currentUsers, setCurrentUsers] = useState(0);
   const [locale, setLocale] = useState("");
+  const [errorLocale, setErrorLocale] = useState(false);
   const [activity, setActivity] = useState("");
   const [social, setSocial] = useState("");
-
-  let preferencesSocial = "";
 
   const submitHandler = (event: any) => {
     event.preventDefault();
     console.log(name);
     console.log(locale);
-    console.log(description);
-    console.log(social);
-    console.log(activity);
     console.log(publico);
+    console.log(startData);
+    console.log(startTime);
+    console.log(finishData);
+    console.log(finishTime);
+    console.log(activity);
+    console.log(social);
+    console.log(maxUsers);
+    console.log(currentUsers);
+    console.log(description);
   };
 
-  //funciona
   const eventTypeHandler = (data: string) => {
     if (data === "Publico") {
       setPublic(true);
@@ -34,6 +45,44 @@ export const NewEvent = () => {
       setPublic(false);
     }
   };
+
+  const eventNameHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    if (!event.currentTarget.value) {
+      setErrorName(true);
+    } else {
+      setErrorName(false);
+    }
+    setName(event.currentTarget.value);
+  };
+
+  const eventLocalHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    if (!event.currentTarget.value) {
+      setErrorLocale(true);
+    } else {
+      setErrorLocale(false);
+    }
+    setLocale(event.currentTarget.value);
+  };
+
+  const eventDescriptionHandler = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    if (!event.currentTarget.value) {
+      setErrorDescription(true);
+    } else {
+      setErrorDescription(false);
+    }
+    setDescription(event.currentTarget.value);
+  };
+
+  /*
+  const eventMaxUsersHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    if (!Number(event.currentTarget.value)) {
+      setErrorMaxUsers(true);
+    } else {
+      setErrorMaxUsers(false);
+    }
+    setMaxUsers(Number(event.currentTarget.value));
+  };
+  */
 
   return (
     <section className="section">
@@ -46,25 +95,15 @@ export const NewEvent = () => {
             <div className="field">
               <label className="label">Nome do Evento</label>
               <div className="control">
-                <input
-                  value={name}
-                  className="input is-primary"
-                  type="text"
-                  placeholder="Event Name"
-                  onChange={(event) => setName(event.currentTarget.value)}
-                />
+                <input value={name} className="input is-primary" type="text" placeholder="Event Name" onChange={eventNameHandler} />
+                {errorName && name.length == 0 && <p className="help is-danger">Nome do evento é necessário</p>}
               </div>
             </div>
             <div className="field">
               <label className="label">Localização</label>
               <div className="control">
-                <input
-                  value={locale}
-                  className="input is-primary"
-                  type="username"
-                  placeholder="Location"
-                  onChange={(event) => setLocale(event.currentTarget.value)}
-                />
+                <input value={locale} className="input is-primary" type="username" placeholder="Location" onChange={eventLocalHandler} />
+                {errorLocale && locale.length == 0 && <p className="help is-danger">Localização é necessária</p>}
               </div>
             </div>
             <label className="label">Tipo de Evento</label>
@@ -79,19 +118,19 @@ export const NewEvent = () => {
             <div className="field is-grouped is-grouped-multiline">
               <div className="p-2">
                 <label className="label">Data de Início</label>
-                <input type="date" />
+                <input type="date" onChange={(event) => setStartData(event.currentTarget.value)} />
               </div>
               <div className="p-2">
                 <label className="label">Hora de Início</label>
-                <input type="time" />
+                <input type="time" onChange={(event) => setStartTime(event.currentTarget.value)} />
               </div>
               <div className="p-2">
                 <label className="label">Data de Fim</label>
-                <input type="date" />
+                <input type="date" onChange={(event) => setFinishData(event.currentTarget.value)} />
               </div>
               <div className="p-2">
                 <label className="label">Hora de Fim</label>
-                <input type="time" />
+                <input type="time" onChange={(event) => setFinishTime(event.currentTarget.value)} />
               </div>
               <div className="p-2">
                 <label className="label">Atividade</label>
@@ -120,14 +159,25 @@ export const NewEvent = () => {
               <div className="p-2">
                 <label className="label">Número Máximo de Participantes</label>
                 <div className="control">
-                  <input className="input is-primary" type="number" placeholder="Number of Participants" />
+                  <input
+                    className="input is-primary"
+                    type="number"
+                    placeholder="Number of Participants"
+                    onChange={(event) => setMaxUsers(Number(event.currentTarget.value))}
+                  />
                 </div>
               </div>
               <div className="p-2">
                 <label className="label">Convidados</label>
                 <div className="field has-addons">
                   <p className="control">
-                    <input id="duration" className="input is-primary" type="username" placeholder="Guests" />
+                    <input
+                      id="duration"
+                      className="input is-primary"
+                      type="username"
+                      placeholder="Guests"
+                      onChange={(event) => setCurrentUsers(Number(event.currentTarget.value))}
+                    />
                   </p>
                   <button type="submit" className="button is-primary">
                     Adicionar
@@ -138,12 +188,8 @@ export const NewEvent = () => {
             <div className="field">
               <label className="label">Descrição</label>
               <div className="control">
-                <textarea
-                  value={description}
-                  className="textarea"
-                  placeholder="Description"
-                  onChange={(event) => setDescription(event.currentTarget.value)}
-                ></textarea>
+                <textarea value={description} className="textarea" placeholder="Description" onChange={eventDescriptionHandler}></textarea>
+                {errorDescription && description.length == 0 && <p className="help is-danger">Descrição é necessária</p>}
               </div>
             </div>
 
