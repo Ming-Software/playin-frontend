2da32cc8e05a72d90b29fb234ae984211ff4e4c4
João Amorim <jonny.amorim2000@gmail.com>
2022-11-23 00:01:49 +0000

    missing files from the other commit

--- src/Pages/Portal/Events.tsx
+++ src/Pages/Portal/Events.tsx
@@ -1,45 +1,47 @@
 export const EventsPage = () => {
   return (
     <section className="section">
-      <div className="card">
-        <div className="card-content">
-          <nav className="breadcrumb" aria-label="breadcrumbs">
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li className="is-active">
-                <a href="#" aria-current="page">
-                  Events
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="content">
-            <ul>
-              <li className="box">
-                <article className="media">
-                  <div className="media-left">
-                    <figure className="eventImage">
-                      <img src="logo-color.png" className="image is-24x24" />
-                    </figure>
-                  </div>
-                </article>
-              </li>
-              <li className="box">
-                <figure className="eventImage">
-                  <img src="logo-color.png" className="image is-16x16" />
-                </figure>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div className="block">
+        <ul>
+          <li className="box">
+            <nav className="level">
+              <div className="level-left">
+                <div className="level-item">
+                  <figure className="eventImage">
+                    <img src="logo-color.png" className="image is-24x24" />
+                  </figure>
+                </div>
+                <p className="level-item">
+                  <strong>Nome do Evento</strong>
+                </p>
+                <p className="level-item ">
+                  <strong>Local: </strong> Braga
+                </p>
+                <p className="level-item">
+                  <strong>Atividade: </strong> Volei
+                </p>
+                <p className="level-item">
+                  <strong>Competitividade: </strong> Social
+                </p>
+                <p className="level-item">
+                  <strong>Dia: </strong> 12/2/2023
+                </p>
+              </div>
+              <div className="level-right">
+                <p className="level-item">
+                  <button className="button is-dark is-samll">Ver</button>
+                </p>
+              </div>
+            </nav>
+          </li>
+          <li className="box">
+            <figure className="eventImage">
+              <img src="logo-color.png" className="image is-16x16" />
+            </figure>
+          </li>
+        </ul>
 
         <nav className="pagination" role="navigation" aria-label="pagination">
-          <a className="pagination-previous" title="This is the first page">
-            Previous
-          </a>
-          <a className="pagination-next">Next page</a>
           <ul className="pagination-list">
             <li>
               <a className="pagination-link is-current" aria-label="Page 1" aria-current="page">
@@ -57,6 +59,10 @@ export const EventsPage = () => {
               </a>
             </li>
           </ul>
+          <a className="pagination-previous" title="This is the first page">
+            Previous
+          </a>
+          <a className="pagination-next">Next page</a>
         </nav>
       </div>
     </section>
