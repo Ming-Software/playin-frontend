0c449acf6fb7e744cc0e5d2c9ff2aff319927e21
felix2000jp <felix2000jp@gmail.com>
2022-12-18 23:04:08 +0000

    topbar is now responsive

--- src/Stores/userStore.tsx
+++ src/Stores/userStore.tsx
@@ -2,10 +2,17 @@ import create from "zustand";
 import UserState from "./userState";
 export const useUserStore = create<UserState>((set) => ({
   email: "",
+  name: "",
   setEmail: (email) => {
     set((state) => ({
       ...state,
       email,
+    }));
+  },
+  setName: (name) => {
+    set((state) => ({
+      ...state,
+      name,
     }));
   },
 }));
