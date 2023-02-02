a4d7bde8ac5cd8a186426db2343aa540a31f0576
ZeVeloso <veloso.j04@gmail.com>
2023-02-02 11:25:04 +0000

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import {
   faUser,
   faArrowUp,
   faArrowDown,
+  faEnvelopeCircleCheck,
 } from "@fortawesome/free-solid-svg-icons";
 library.add(
   fab,
@@ -33,7 +34,8 @@ library.add(
   faStar,
   faUser,
   faArrowUp,
-  faArrowDown
+  faArrowDown,
+  faEnvelopeCircleCheck
 );
 
 import LoginPage from "./Pages/Login";
